aa0cfda5d3ba6349434cf9d273fe956643a205ca
sophia00000000 <laura.hernandez10@usa.edu.co>
2024-11-21 07:21:09 -0500

    Update Radix.java

--- radix/src/radix/Radix.java
+++ radix/src/radix/Radix.java
@@ -3,62 +3,65 @@ package radix;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
-import java.util.Random;
+
 
 public class Radix {
-	public static void main(String[] args) throws Exception {
-        Random random =new Random();
-
-        List<Integer> lista =new ArrayList<>();
-
+	
+	
+    public static ArrayList<Integer> ordenarRadix(ArrayList<Integer> listaAux){
+    	
+    	ArrayList<Queue<Integer>> cubetas =new ArrayList<>();
+        //inseratar 10 colas/cubetas a la lista 
         for (int i = 0; i < 10; i++) {
-            lista.add(random.nextInt(9999-1000+1));
+            cubetas.add(new LinkedList<>());
         }
-        System.out.println("Lista Original");
-        System.out.println(lista.toString());
-        ordenarLista(lista);
-    }
-
-    public static List<Integer> ordenarLista(List<Integer> lista){
-        List<Queue<Integer>> contenedor =new ArrayList<>();
-        //Creo 10 colas y las referencio a cada elemento de la lista de coloas contenedor
-        for (int i = 0; i < 10; i++) {
-            contenedor.add(new LinkedList<>());
-        }
-        //Busca el mayor dato en el arreglo
-        int mayor = Collections.max(lista);
-        System.out.println("El número mayor es: " + mayor);
-        //        
+        //encontrar el mayor
+        int mayor = Collections.max(listaAux);
         int pasada = (int) Math.floor(Math.log10(Math.abs(mayor)) + 1);
 
-        System.out.println("Numero Mayor "+mayor+" Número de Pasadas para Ordenar"+pasada);     
-        int numero;
-        int digito;
+        System.out.println("mayor: "+mayor);
+        System.out.println("numero de claves: " +pasada); 
+        
+        
+        int num;
+        int digit;
         int j=1;
+        
         for (int i = 0; i < pasada; i++) {
-
-            while(!lista.isEmpty()) {
-                numero = lista.remove(0);
-
-                int n = numero/j;
-                digito = n%10;
-                contenedor.get(digito).add(numero);
+            while(!listaAux.isEmpty()) {
+            	//Distribuir Números en las Colas Según el Dígito Actual
+                num = listaAux.remove(0);
+                int n = num/j;
+                digit = n%10;
+                cubetas.get(digit).add(num);
             }
             j*=10;
 
             
-            System.out.println(contenedor.toString());
-            for (Queue cola: contenedor) {
+            System.out.println(cubetas.toString());
+            //vaciar las cubetas y llenar la lista
+            for (Queue<Integer> cola: cubetas) {
                 while(!cola.isEmpty())
-                   lista.add((Integer)cola.poll());
+                   listaAux.add((Integer)cola.poll());
             }
-            System.out.println((i+1)+" Pasasa");
-            System.out.println(lista.toString());
+            System.out.println((i+1)+" Iteración");
+            System.out.println(listaAux.toString());
        }
-       return lista;
+       return listaAux;
 
     }
+    
+    public static void main(String[] args){
+        
+    	ArrayList<Integer> listaAux =new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            listaAux.add((int) (Math.random() * 9000) + 1000);
+        }
+        System.out.println("Lista original:");
+        System.out.println(listaAux.toString());
+        ordenarRadix(listaAux);
+    }
+    
 
 }
